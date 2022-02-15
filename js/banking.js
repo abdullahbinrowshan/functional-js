f6c5680b873276934b737a1547bebe3ed4bb616a
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2022-02-15 17:41:39 +0600

    nested function and error handling

--- js/banking.js
+++ js/banking.js
@@ -15,10 +15,17 @@ function updateTotalField(totalFieldId, amount) {
     totalElement.innerText = previousTotal + amount;
 }
 
-function updateBalance(amount, isAdd) {
+function getCurrentBalance() {
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceText);
+    return previousBalanceTotal;
+}
+
+function updateBalance(amount, isAdd) {
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = getCurrentBalance();
 
     if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
@@ -28,17 +35,31 @@ function updateBalance(amount, isAdd) {
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
+
     const depositAmount = getInputValue('deposit-input');
+    const currentBalance = getCurrentBalance();
 
-    updateTotalField('deposit-total', depositAmount)
-
-    updateBalance(depositAmount, true)
+    if (depositAmount > 0 && currentBalance < depositAmount) {
+        updateTotalField('deposit-total', depositAmount)
+        updateBalance(depositAmount, true)
+    } else if (depositAmount > currentBalance) {
+        console.log('you can not withdraw more than what you have in your account')
+    } else if (depositAmount <= 0) {
+        console.log('You have to deposit valid money');
+    }
 })
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
+
     const withdrawAmount = getInputValue('withdraw-input')
+    const currentBalance = getCurrentBalance();
 
-    updateTotalField('withdraw-total', withdrawAmount)
-
-    updateBalance(withdrawAmount, false)
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount)
+        updateBalance(withdrawAmount, false)
+    } else if (withdrawAmount > currentBalance) {
+        console.log('you can not withdraw more than what you have in your account')
+    } else if (withdrawAmount <= 0) {
+        console.log('You have to withdraw valid money');
+    }
 })
